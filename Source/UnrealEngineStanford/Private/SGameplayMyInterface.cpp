f66dc1e5a519af1c563d0d5c8183ad84a3566469
Bardia Hassanzadeh <bardia@ualberta.ca>
2022-11-20 02:09:08 -0700

    Added a bull-cow-like game prototype
    Added a console like prototype to simulate a bull-cow guessing password game prototype.

---
+++ Source/UnrealEngineStanford/Private/SGameplayMyInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGameplayMyInterface.h"
+
+// Add default functionality here for any ISGameplayMyInterface functions that are not pure virtual.
